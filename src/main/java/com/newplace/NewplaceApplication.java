eed8f0ffb7f2a95e6fc8b040c0ef0dc4f2952a06
JinAh Kwak <58837220+saySthAbout@users.noreply.github.com>
2023-05-17 14:17:38 +0900

    Initial commit

---
+++ src/main/java/com/newplace/NewplaceApplication.java
@@ -0,0 +1,13 @@
+package com.newplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newplace/NewplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
